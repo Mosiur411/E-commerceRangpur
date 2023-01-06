9e8da3a1654a8f0574149f8adbb3ef387e2fab21
Mosiur <mosiurislamwebdesign@gmail.com>
2023-01-06 11:51:36 +0600

    devloy

--- app/features/api/ProductControl.jsx
+++ app/features/api/ProductControl.jsx
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-const URL = 'http://localhost:3000/';
+const URL = 'https://e-commerce-rangpur.vercel.app/';
 export const ProductControl = createApi({
     reducerPath: "Product",
     baseQuery: fetchBaseQuery({
